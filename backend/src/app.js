3c25a359fc2760f246d594bf3c6b743e0a2d1578
Aayush Raj <raj.aaayush96@gmail.com>
2018-10-03 09:50:11 +0530

    backend status working

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+app.listen(8081, function () {
+  console.log('App serving at Port 8081')
+})
